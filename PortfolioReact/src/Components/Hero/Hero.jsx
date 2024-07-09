fe67260c4e918c4076595bb4940fd804861ee27b
edgardo <edgardodelreal@gmail.com>
2024-07-09 17:30:45 -0300

    Defini algunos componentes, inicio el proyecto

---
+++ PortfolioReact/src/Components/Hero/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Hero.css'
+const Hero = () =>{
+    return(
+        <div>
+            <p>Hola mundo</p>
+        </div>
+    )
+}
+
+export default Hero 
--- PortfolioReact/src/main.jsx
+++ PortfolioReact/src/main.jsx
@@ -1,8 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import NavBar from './Components/NavBar/NavBar.jsx'
+import Hero from './Components/Hero/Hero.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <NavBar/>
+    <Hero/>
     <App />
   </React.StrictMode>,
 )
