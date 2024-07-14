72ac54e830285875332504b8aa3c4bcc15e4243a
edgardo <edgardodelreal@gmail.com>
2024-07-14 03:30:34 -0300

    Termine el que hago. Tengo que reveer algunos temas con la etiqueta flex

--- PortfolioReact/src/Components/QueHago/QueHago.jsx
+++ PortfolioReact/src/Components/QueHago/QueHago.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
-import './QueHago.css'
+import './QueHago.css';
+import pic1 from '../../assets/pic1.png';
+import pic2 from '../../assets/pic2.png'
+import {Box, Center, Flex} from '@chakra-ui/react'
 const QueHago = () =>{
     return(
-        <div>
-            <p>Hola mundo</p>
-        </div>
+        <Box className="que-hago">
+                <Flex justifyContent='center' alignItems='center' flexWrap='wrap'>
+                <h2><span className="signos">&lt;</span>¿Que hago?<span className="signos">&gt;</span></h2>
+                <Box as='section' className="card-desarrollo-web">
+                    <Box as='section' className="contenedor-card1">
+                        <img src={pic1} alt=""/>
+                        <h2 className="titulo-card">Desarrollo Web</h2>
+                    </Box>
+                </Box>
+                <Box as='section' className="card-desarrollo-software">
+                    <Box as='section' className="contenedor-card2">
+                        <img src={pic2} alt=""/>
+                        <h2 className="titulo-card">Desarrollo Softaware</h2>
+                    </Box>  
+                </Box>
+                </Flex>
+        </Box>
     )
 }
 
--- PortfolioReact/src/main.jsx
+++ PortfolioReact/src/main.jsx
@@ -20,6 +20,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Hero/>
       <Projectos/>
       <AboutMe/>
+      <QueHago/>
       <App/>
     </ChakraProvider>
   </React.StrictMode>,
